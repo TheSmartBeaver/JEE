b9d29f8c236c71a42800239acf8ee8ea9aeb3afb
BergerPaul <totorx36bhu@gmail.com>
2020-04-25 08:56:12 +0200

    supprimer classe vraiment inutiles

--- Projet/ens-SpringBoot/src/main/java/mybootapp/web/GroupController.java
+++ Projet/ens-SpringBoot/src/main/java/mybootapp/web/GroupController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.validation.Valid;
-import mybootapp.business.IProductManager;
 import mybootapp.dao.DAOPerson;
 import mybootapp.model.Party;
 import mybootapp.model.Product;
